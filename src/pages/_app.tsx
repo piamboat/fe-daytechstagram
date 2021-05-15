8ac709a9e169e125674d44635059a012a6a965e2
piamsak <piamsakboat@gmail.com>
2021-05-15 12:34:24 +0700

    add transition between page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,21 @@ import 'antd/dist/antd.css';
 import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app';
 import { PageHeader } from 'antd';
+import { motion, MotionConfig } from 'framer-motion';
 import React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <React.Fragment>
+    <motion.div key={ router.route } initial="pageInit" animate="pageAnimate" variants={{
+      pageInit: { opacity: 0 },
+      pageAnimate: { opacity: 1 },
+    }}>
       <PageHeader
         title="Daytechstagram"
         subTitle="Share your happiness"
       />
       <Component {...pageProps} />
-    </React.Fragment>
+    </motion.div>
   )
 }
 
