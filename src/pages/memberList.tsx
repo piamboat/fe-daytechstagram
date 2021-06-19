1c16369cd7715bc050c57eec4a13f8be053faeec
piamsak <piamsakboat@gmail.com>
2021-06-19 19:56:05 +0700

    add ui progress to memberList

--- src/pages/memberList.tsx
+++ src/pages/memberList.tsx
@@ -1,5 +1,6 @@
 import { Layout, Menu, Breadcrumb } from "antd";
 import {
+  GitlabOutlined,
   UserOutlined,
   LineChartOutlined,
   TeamOutlined,
@@ -10,59 +11,77 @@ const { Header, Content, Sider } = Layout;
 
 const memberList: React.FC = () => {
   return (
-    <Layout>
-      <Header
-        className="header"
-        style={{ height: "100%", borderRight: 0, background: "#F0F0F0" }}
-      >
-        <div className="logo" />
+    <Layout
+      className="min-h-screen"
+      style={{ height: "100%", borderRight: 0, background: "#F0F0F0" }}
+    >
+      <Sider>
         <Menu
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
+          mode="inline"
+          defaultOpenKeys={["sub4"]}
           style={{ height: "100%", borderRight: 0, background: "#F0F0F0" }}
         >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item
+            key="sub1"
+            icon={<GitlabOutlined style={{ fontSize: "125%" }} />}
+            style={{ fontSize: "24px", color: "#001529" }}
+          >
+            Logo
+          </Menu.Item>
+          <Menu.Item
+            key="sub2"
+            icon={<LineChartOutlined style={{ fontSize: "125%" }} />}
+            style={{ fontSize: "16px", color: "#001529" }}
+          >
+            Dashboard
+          </Menu.Item>
+          <SubMenu
+            key="sub3"
+            icon={<UserOutlined style={{ fontSize: "125%" }} />}
+            title="Staff"
+            style={{ fontSize: "16px", color: "#001529" }}
+          >
+            <Menu.Item key="1">Staff management</Menu.Item>
+            <Menu.Item key="2">Role management</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub4"
+            icon={<TeamOutlined style={{ fontSize: "125%" }} />}
+            title="Member"
+            style={{ fontSize: "16px", color: "#001529" }}
+          >
+            <Menu.Item key="3">Member management</Menu.Item>
+            <Menu.Item key="4">Group management</Menu.Item>
+            <Menu.Item key="5">Tags management</Menu.Item>
+            <Menu.Item key="6">Member info</Menu.Item>
+          </SubMenu>
         </Menu>
-      </Header>
-      <Layout className="min-h-screen">
-        <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultOpenKeys={["sub2"]}
-            style={{ height: "100%", borderRight: 0, background: "#F0F0F0" }}
-          >
-            <Menu.Item key="1" icon={<LineChartOutlined />}>
-              Dashboard
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="Staff">
-              <Menu.Item key="1">Staff management</Menu.Item>
-              <Menu.Item key="2">Role management</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Member">
-              <Menu.Item key="3">Member management</Menu.Item>
-              <Menu.Item key="4">Group management</Menu.Item>
-              <Menu.Item key="5">Tags management</Menu.Item>
-              <Menu.Item key="6">Member info</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout style={{ background: "white", padding: "0 24px 24px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }} separator=">">
-            <Breadcrumb.Item>Member</Breadcrumb.Item>
-            <Breadcrumb.Item>Member management</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
+      </Sider>
+      <Layout style={{ background: "white" }}>
+        <Menu
+          mode="horizontal"
+          style={{ background: "#F0F0F0", fontSize: "16px", color: "#001529" }}
+        >
+          <Menu.Item key="1">Customer Service</Menu.Item>
+          <Menu.Item key="2">Marketing</Menu.Item>
+        </Menu>
+        <Breadcrumb
+          style={{ margin: "16px 0", padding: "0 24px 24px" }}
+          separator=">"
+        >
+          <Breadcrumb.Item>Member</Breadcrumb.Item>
+          <Breadcrumb.Item>Member management</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
       </Layout>
     </Layout>
   );
